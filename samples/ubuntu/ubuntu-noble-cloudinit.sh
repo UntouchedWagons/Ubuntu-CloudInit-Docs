101bff6fcb4245da1c384d6912bbb808bbab4fd8
John DeRose <jderose@jderose.net>
2025-01-22 12:24:47 -0700

    updated --cpu and --net0 specs to match README.md. tested on PVE 8.3.0.

--- samples/ubuntu/ubuntu-noble-cloudinit.sh
+++ samples/ubuntu/ubuntu-noble-cloudinit.sh
@@ -12,9 +12,9 @@ sudo qm create $VMID --name "ubuntu-noble-template" --ostype l26 \
     --memory 1024 --balloon 0 \
     --agent 1 \
     --bios ovmf --machine q35 --efidisk0 $STORAGE:0,pre-enrolled-keys=0 \
-    --cpu host --cores 1 --numa 1 \
+    --cpu host --socket 1 --cores 1 \
     --vga serial0 --serial0 socket  \
-    --net0 virtio,bridge=vmbr0,mtu=1
+    --net0 virtio,bridge=vmbr0
 sudo qm importdisk $VMID noble-server-cloudimg-amd64.img $STORAGE
 sudo qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
 sudo qm set $VMID --boot order=virtio0
