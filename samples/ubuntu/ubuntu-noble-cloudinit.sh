d94eefa800d0edd9962cc96fb03eaed67356f090
Henry Zhang <zeri@umich.edu>
2025-11-27 15:33:34 -0500

    Add image verification and not-in-place resizing
    Adds functionality to download, checksum-verify, and resize Ubuntu Noble
    cloud images (not in place) before importing them into Proxmox.

--- samples/ubuntu/ubuntu-noble-cloudinit.sh
+++ samples/ubuntu/ubuntu-noble-cloudinit.sh
@@ -1,13 +1,38 @@
 #! /bin/bash
 
-VMID=8200
-STORAGE=local-zfs
+set -xe
 
-set -x
-rm -f noble-server-cloudimg-amd64.img
-wget -q https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
-qemu-img resize noble-server-cloudimg-amd64.img 8G
-sudo qm destroy $VMID
+VMID="${VMID:-8200}"
+STORAGE="${STORAGE:-local-zfs}"
+
+IMG="noble-server-cloudimg-amd64.img"
+BASE_URL="https://cloud-images.ubuntu.com/noble/current"
+EXPECTED_SHA=$(wget -qO- "$BASE_URL/SHA256SUMS" | awk '/'$IMG'/{print $1}')
+
+download() {
+    wget -q "$BASE_URL/$IMG"
+}
+
+verify() {
+    sha256sum "$IMG" | awk '{print $1}'
+}
+
+[ ! -f "$IMG" ] && download
+
+ACTUAL_SHA=$(verify)
+
+if [ "$EXPECTED_SHA" != "$ACTUAL_SHA" ]; then
+    rm -f "$IMG"
+    download
+    ACTUAL_SHA=$(verify)
+    [ "$EXPECTED_SHA" != "$ACTUAL_SHA" ] && exit 1
+fi
+
+rm -f noble-server-cloudimg-amd64-resized.img
+cp noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64-resized.img
+qemu-img resize noble-server-cloudimg-amd64-resized.img 8G
+
+sudo qm destroy $VMID || true
 sudo qm create $VMID --name "ubuntu-noble-template" --ostype l26 \
     --memory 1024 --balloon 0 \
     --agent 1 \
@@ -15,7 +40,7 @@ sudo qm create $VMID --name "ubuntu-noble-template" --ostype l26 \
     --cpu host --socket 1 --cores 1 \
     --vga serial0 --serial0 socket  \
     --net0 virtio,bridge=vmbr0
-sudo qm importdisk $VMID noble-server-cloudimg-amd64.img $STORAGE
+sudo qm importdisk $VMID noble-server-cloudimg-amd64-resized.img $STORAGE
 sudo qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
 sudo qm set $VMID --boot order=virtio0
 sudo qm set $VMID --scsi1 $STORAGE:cloudinit
